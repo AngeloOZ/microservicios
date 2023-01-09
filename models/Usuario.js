989355750c3c3e026513cf4dad0e8f9d2a9afd9f
AngeloOz <angelo-mjz@hotmail.com>
2023-01-08 21:23:07 -0500

    fix: usuario add unique

--- models/Usuario.js
+++ models/Usuario.js
@@ -10,6 +10,7 @@ const Usuario = sequelize.define('Usuario', {
     usuario: {
         type: DataTypes.STRING(50),
         allowNull: false,
+        unique: true,
     },
     contrasenia: {
         type: DataTypes.STRING(150),
@@ -22,6 +23,7 @@ const Usuario = sequelize.define('Usuario', {
     identificacion: {
         type: DataTypes.STRING(15),
         allowNull: false,
+        unique: true
     },
     nombre: {
         type: DataTypes.STRING(100),
